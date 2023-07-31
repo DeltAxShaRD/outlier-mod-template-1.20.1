9ecabd489811cc820c09125d90e19f27d13d9b1e
delta <delta.f.a.shard@gmail.com>
2023-07-31 12:35:41 -0600

    Let's try this again.

---
+++ src/main/java/ariafey/outlier/OutlierModClient.java
@@ -0,0 +1,10 @@
+package ariafey.outlier;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class OutlierModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/ariafey/outlier/OutlierModDataGenerator.java
@@ -0,0 +1,11 @@
+package ariafey.outlier;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class OutlierModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
